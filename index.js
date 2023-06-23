0015768e9925359704d32b3d1ee719b568421445
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-23 22:16:24 +0300

    add move left functon and event

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+//move dodger left function and event
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace(`px`, ``)
-    const left = parseInt(leftNumbers)
+    const leftNumbers = dodger.style.left.replace(`px`, ``);
+    const left = parseInt(leftNumbers);
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`
-    }}
-
-    document.addEventListener(`keydown`, function(e) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+  
+  document.addEventListener(`keydown`, function(e) {
     if (e.key === `ArrowLeft`) {
-        moveDodgerLeft()
-    }})
+      moveDodgerLeft();
+    }
+})
 
+//move dodger right function and event
+function moveDodgerRight() {
+    const rightMeasurement = dodger.style.left.replace(`px`, ``)
+    const right = parseInt(rightMeasurement)
+    if (left < 40) {
+        dodger.style.left = `${left + 1}`
+    }
+}
 
-    
+  document.addEventListener(`keydown`, function(event){
+    if (event.key === `ArrowRight`){
+        moveDodgerRight()
+    }
+  })
 
