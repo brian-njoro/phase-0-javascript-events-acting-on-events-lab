3981b4b4cd78ae6ca014156c25f2f60bff6d1c48
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-23 22:18:41 +0300

    add corrections to index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function moveDodgerLeft() {
 
 //move dodger right function and event
 function moveDodgerRight() {
-    const rightMeasurement = dodger.style.left.replace(`px`, ``)
-    const left = parseInt(rightMeasurement)
+    const leftNumbers = dodger.style.left.replace(`px`, ``)
+    const left = parseInt(leftNumbers)
     if (left < 40) {
         dodger.style.left = `${left + 1}`
     }
